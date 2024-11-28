179fbd0f8c85da5d93ef3a208f246c0d8d278709
JEmanuel1806 <janplischke97@gmail.com>
2024-11-28 15:13:33 +0100

    Initial commit

---
+++ mod.lua
@@ -0,0 +1,169 @@
+﻿function data()
+  return {  
+	info = {
+		minorVersion = 0,
+		severityAdd = "NONE",
+		severityRemove = "WARNING", 
+		name = _("mod_name"),
+		description = _(
+						"mod_desc"
+						),
+		authors = {
+			{
+				name = 'jay_',
+				role = 'CREATOR',
+				text = 'Modell',
+				tfnetId = 28954,
+			},
+					    {
+		        name = "ModWerkstatt",
+		        role = "CREATOR",
+		    },  
+		},
+		params = {		
+			{
+				key = "zugschluss_land",
+				name = _("tafel_aktiv"),
+				values = {_("schluss_de"), _("schluss_fr"),},
+				tooltip = _("schluss_tooltip"),
+				defaultIndex = 0,
+			},
+			{
+				key = "ctc_versionen",
+				name = _("ctc_aktiv"),
+				values = {_("yes"), _("no"),},
+				tooltip = _("ctc_tooltip"),
+				defaultIndex = 0,
+			},
+			{
+				key = "fr_versionen",
+				name = _("fr_aktiv"),
+				values = {_("yes"), _("no"),},
+				tooltip = _("frVersion_tooltip"),
+				defaultIndex = 0,
+			},
+						{
+				key = "it_versionen",
+				name = _("it_aktiv"),
+				values = {_("yes"), _("no"),},
+				tooltip = _("itVersion_tooltip"),
+				defaultIndex = 0,
+			},
+			
+			{
+				key = "soundCheck",
+				name = _("sound_check"),
+				uiType = "CHECKBOX",
+				values = { "No", "Yes", },				
+				defaultIndex = 1,	
+			},
+		},
+		tags = { "Europe", "Vehicle", "Freight", "Wagon", "CTC", "Transcereales", "Hopper", "Uagps", },
+		dependencies = {},
+		}, 
+		
+		runFn = function (settings, modParams)
+		local params = modParams[getCurrentModId()]
+
+		
+		-- Filter the foldersssssssssss
+		
+		local function excludeFolderFilter_german(fileName, data)
+			print(fileName)
+			if fileName:match('/schluss_fr/base/') or fileName:match('/schluss_fr/ctc/') or fileName:match('/schluss_fr/fra/') or fileName:match('/schluss_fr/ita/') then
+				return false 
+			end
+			return true 
+		end
+		
+		local function excludeFolderFilter_french(fileName, data)
+			print(fileName)
+			if fileName:match('/schluss_de/base/') or fileName:match('/schluss_de/ctc/') or fileName:match('/schluss_de/fra/') or fileName:match('/schluss_de/ita/') then
+				return false 
+			end
+			return true 
+		end
+		
+		-- CTC Filter
+		
+		local function excludeFolderFilter_ctc(fileName, data)
+			print(fileName)
+			if fileName:match('/schluss_fr/ctc/') or fileName:match('/schluss_de/ctc/') then
+				return false 
+			end
+			return true 
+		end
+		
+		-- Filter france
+		
+		local function excludeFolderFilter_france(fileName, data)
+			print(fileName)
+			if fileName:match('/schluss_fr/fra/') or fileName:match('/schluss_de/fra/') then
+				return false 
+			end
+			return true 
+		end
+		
+		-- Filer italia
+		
+		local function excludeFolderFilter_italy(fileName, data)
+			print(fileName)
+			if fileName:match('/schluss_fr/ita/') or fileName:match('/schluss_de/ita/') then
+				return false 
+			end
+			return true 
+		end
+		
+		if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]		
+			-- zugschluss
+			if params["zugschluss_land"] == 0 then
+				addFileFilter("model/vehicle", excludeFolderFilter_german)		
+			end
+			if params["zugschluss_land"] == 1 then
+				addFileFilter("model/vehicle", excludeFolderFilter_french)		
+			end
+			-- ctc
+			if params["ctc_versionen"] == 1 then
+				addFileFilter("model/vehicle", excludeFolderFilter_ctc)		
+			end
+			-- france
+			if params["fr_versionen"] == 1 then
+				addFileFilter("model/vehicle", excludeFolderFilter_france)		
+			end
+			-- italy
+			if params["it_versionen"] == 1 then
+				addFileFilter("model/vehicle", excludeFolderFilter_italy)		
+			end
+			
+		else
+			addFileFilter("model/vehicle", modelFilter)	
+		end	
+		
+
+		
+
+		local function metadataHandler(fileName, data)
+			if params.soundCheck == 0 then
+			local waggon_types = { "uagpsA", "uagpsB" }
+			local zugschluss = { "schluss_de", "schluss_fr" }
+			local versions = { "base", "ctc", "fra", "ita" }
+
+			for _, waggon_type in ipairs(waggon_types) do
+				for _, schluss in ipairs(zugschluss) do
+					for _, version in ipairs(versions) do
+						if fileName:match('/vehicle/waggon/' .. waggon_type .. '/' .. schluss .. '/' .. version .. '/' .. waggon_type .. '_([^/]*.mdl)') then
+							data.metadata.railVehicle.soundSet.name = "waggon_freight_modern"
+							break
+						end
+					end
+				end
+			end
+		end
+		return data
+	end
+
+		addModifier( "loadModel", metadataHandler )
+	end,
+}
+end
---
+++ strings.lua
@@ -0,0 +1,75 @@
+function data()
+return {
+	en = {
+
+		["mod_name"] = "Uagp(p)s Universal Grain Hopper",
+		["mod_desc"] = " Uagps is a four-axle universal grain wagon primarily operated by the Compagnie de Transport de Céréales (CTC). Manufactured by Fauvet Girel from the 1970s onwards, it was designed for the transport of grain and other bulk goods. The wagons feature roof hatches for loading and side discharge flaps for unloading. Variants with flat or curved sidewalls exist, depending on the year of construction and series. The Uagps was leased multiple times to various companies and continues to be used flexibly in international freight transport today. Following CTC's acquisition by Ermewa in 2016, many wagons transitioned over time to Ermewa, VTG, Interfracht, and Millet.",
+
+		["uagpsA1_desc"] = "Universal grain wagon Uagps, built in 1971 by Fauvet Girel. Version with flat sidewalls, limited to 100 km/h.",
+		["uagpsB1_desc"] = "Universal grain wagon Uagps, built in 1974 by Fauvet Girel. Version with curved sidewalls.",
+
+		
+		["height"] = "Height",
+		["height_tooltip"] = "Configure height",
+		["jrm_param_offsetx_tooltip"] = "Configure offset",
+		["jrm_param_type_tooltip"] = "Type",
+		["offset x-axis"] = "Offset x-axis",
+		["ground"] = "Ground",
+		["rail"] = "Rail",
+		["vehicle"] = "Vehicle",
+		
+		["UagpsA"] = "Uagps (flat sidewalls)",
+		["UagpsB"] = "Uagps (curved sidewalls)",
+		
+		["tafel_aktiv"] = "Type of Train End",
+		["schluss_de"] = "Train End Board",
+		["schluss_fr"] = "Train End Light",
+		["schluss_tooltip"] = "Choose between train end boards or train end lights (SNCF, SNCB..)",
+		
+		["ctc_aktiv"] = "Additional C.T.C. Versions",
+		["fr_aktiv"] = "Additional French Versions",
+		["it_aktiv"] = "Italian Versions",
+		["ctc_tooltip"] = "Enable additional Transcereales liveries from various operators",
+		["frVersion_tooltip"] = "Enable additional French liveries",
+		["itVersion_tooltip"] = "Enable Italian liveries",
+		
+		["sound_check"] = "Activate Additional Soundset"
+
+	},
+	de = {
+
+		["mod_name"] = "Universalgetreidewagen Uagp(p)s",
+		["mod_desc"] = "Uagps ist ein vierachsiger Universalgetreidewagen, der von der Compagnie de Transport de Céréales (CTC) vorrangig verwendet wurde. Er wurde ab den 1970er-Jahren von Fauvet Girel gefertigt und für den Transport von Getreide und anderen Schüttgütern entwickelt. Die Wagen sind mit Klappen auf dem Dach für das Befüllen und seitlichen Schüttklappen für die Entladung ausgestattet. Es gibt Varianten mit flachen und gewölbten Seitenwänden, die je nach Baujahr und Serie variieren. Der Uagps wurde mehrfach an verschiedene Unternehmen vermietet und flexibel bis heute im internationalen Güterverkehr eingesetzt. CTC wurde 2016 von Ermewa übernommen, zahlreiche Wagen gingen über die Zeit somit an Ermewa, VTG, Interfracht und Millet.",
+
+        ["uagpsA1_desc"] = "Universalgetreidewagen Uagps, gebaut 1971 von Fauvet Girel. Version mit geraden Seitenwänden, beschränkt auf 100 km/h.",
+        ["uagpsB1_desc"] = "Universalgetreidewagen Uagps, gebaut 1974 von Fauvet Girel. Version mit gewölbten Seitenwänden.",
+		
+		["height"] = "Höhe",
+		["height_tooltip"] = "Höhe einstellen",
+		["jrm_param_offsetx_tooltip"] = "Versatz einstellen",
+		["jrm_param_type_tooltip"] = "Typ",
+		["offset x-axis"] = "Verschub X-Achse",
+		["ground"] = "Boden",
+		["rail"] = "Gleis",
+		["vehicle"] = "Fahrzeug",
+		
+		["UagpsA"] = "Uagps (gerade Seitenwände)",
+		["UagpsB"] = "Uagps (gewölbte Seitenwände)",
+		
+		["tafel_aktiv"] = "Art von Zugschluss",
+		["schluss_de"] = "Zugschlusstafel",
+		["schluss_fr"] = "Zugschlusslicht",
+		["schluss_tooltip"] = "Entweder Zugschlusstafeln oder Zugschlussleuchten (SNCF, SNCB..) wählen",
+		
+		["ctc_aktiv"] = "Zusätzliche C.T.C. Versionen",
+		["fr_aktiv"] = "Zusätzliche Französische Versionen",
+		["it_aktiv"] = "Italienische Versionen",
+		["ctc_tooltip"] = "Aktivieren zusätzlicher Transcereales Lackierungen von verschiedenen Einstellern",
+		["frVersion_tooltip"] = "Aktivieren zusätzlicher französischer Lackierungen",
+		["itVersion_tooltip"] = "Aktivieren italienischer Lackierungen",
+		
+		["sound_check"] = "Zusätzliches Soundset aktivieren"
+		
+	},
+}
+end
